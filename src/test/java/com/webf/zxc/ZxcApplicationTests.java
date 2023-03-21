fde417354af0733c30691ca61bb62f6fedfe2631
fgbda dbg <aryemfedorov@gmail.com>
2023-03-21 20:23:03 +0200

    kdfdsfk

---
+++ src/test/java/com/webf/zxc/ZxcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webf.zxc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZxcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
